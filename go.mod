e9d30d891ff556378ae94e4ab108cda3125e1caf
Martin Gysel <me@bearsh.org>
2022-06-27 14:27:50 +0200

    req go1.17, migrate to go1.17+ build constraints

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/bearsh/hid
 
-go 1.14
+go 1.17
--- hidapi/hidapi.go
+++ hidapi/hidapi.go
@@ -1,4 +1,4 @@
-// +build dummy
+//go:build dummy
 
 // This Go file is part of a workaround for `go mod vendor`.
 // Please see the file `c_deps.go` at the root for more information.
